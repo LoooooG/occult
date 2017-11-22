06c6bfa72e91326bfcb528ac97551944ad98afc1
LoooooG <53099003@qq.com>
2017-11-22 16:56:33 +0800

    链接打开App，微信跳应用宝处理

--- src/components/common/DownloadOrOpen.vue
+++ src/components/common/DownloadOrOpen.vue
@@ -1,16 +1,21 @@
 <script>
+    import Global from '@/global/Global'
     import Url from '@/global/Url'
 
     export default {
         methods: {
             go() {
-                location.href = Url.urlList.URL_START_APP
-                setTimeout(() => {
+                if (Global.isWeiXin()) {
                     location.href = Url.urlList.URL_TECENT_DOWNLOAD
-                }, 250)
-                setTimeout(() => {
-                    location.reload()
-                }, 1000)
+                } else {
+                    location.href = Url.urlList.URL_START_APP
+                    setTimeout(() => {
+                        location.href = Url.urlList.URL_TECENT_DOWNLOAD
+                    }, 250)
+                    setTimeout(() => {
+                        location.reload()
+                    }, 1000)
+                }
             }
         }
     }
